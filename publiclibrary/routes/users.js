7eff6e68b70f84462a66cba52c9131ec87bb90be
Tuyen Dang <tuyendq@gmail.com>
2020-03-15 22:16:36 +0700

    Part 2: Creating a skeleton webste. Finished

--- publiclibrary/routes/users.js
+++ publiclibrary/routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/**
+ * Challenge GET /users/cool/
+ */
+router.get('/cool/', function(req, res, next) {
+  res.send('Public Library is so cool!');
+});
+
 module.exports = router;
